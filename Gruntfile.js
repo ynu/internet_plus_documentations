0443b85d31d80e52b2b58fc850b38a0e21360447
liudonghua123 <liudonghua123@gmail.com>
2015-08-06 16:06:21 +0800

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    // configurable paths
+    grunt.initConfig({
+
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        'dist'
+                    ]
+                }]
+            }
+        },
+
+        shell: {
+            dist: {
+                command: 'gitbook build . ./dist'
+            },
+        },
+
+        'gh-pages': {
+          options: {
+            base: 'dist'
+          },
+          src: ['**']
+        }
+
+
+
+    });
+
+    grunt.registerTask('deploy', ['clean', 'shell', 'gh-pages']);
+    grunt.registerTask('default', ['deploy']);
+};
